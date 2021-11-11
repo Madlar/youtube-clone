8ca64b208827238d90d05ce4dfe2e22b17abeb6c
MadLar <pjcj1@naver.com>
2021-11-11 15:05:07 +0900

    비디오 업로드 페이지 버그 수정
    드랍존 내부 +가 가운데 정렬이 안되던 버그 수정

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -74,7 +74,7 @@ function VideoUploadPage() {
                     maxSize={1000000000}
                     >
                     {({ getRootProps, getInputProps}) => (
-                        <div style={{ width: '300px', height: '240px', border: '1px solid lightgray',
+                        <div style={{ display: 'flex', width: '300px', height: '240px', border: '1px solid lightgray',
                         alignItems: 'center', justifyContent: 'center'}} {...getRootProps()}>
                             <input {...getInputProps()} />
                             <Icon type='plus' style={{ fontSize: '3rem'}}/>
